ae970843b5a2fbb027fa06df096b368804150f8b
Chintan <bat.boy18009@gmail.com>
2026-04-30 00:20:25 +0530

    New
    New

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Chintan_Vadgama_Products-CV_VFX_Demo-0.13";
+const contentToCache = [
+    "Build/VFX_Demo.loader.js",
+    "Build/VFX_Demo.framework.js.unityweb",
+    "Build/VFX_Demo.data.unityweb",
+    "Build/VFX_Demo.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
